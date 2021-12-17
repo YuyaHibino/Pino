fbbe73fb6bc3dd78080a807c448cd20a357c2062
YuyaHibino <yu10191999oct@gmail.com>
2021-12-17 21:39:25 +0900

    カリキュラム8-4ブログ投稿

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ Route::get('/', function() {
 });
 
 Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
+
+Route::post('/posts', 'PostContller@store');
